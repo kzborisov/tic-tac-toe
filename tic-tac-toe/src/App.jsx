548a087cab84bd812620267659f266eb95e5fa92
kborisov <kborisov@lely.com>
2023-04-19 13:00:49 +0300

    Initial setup

---
+++ tic-tac-toe/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+    return (
+        <div className="">
+            <h1>Tic Tac Toe</h1>
+        </div>
+    )
+}
+
+export default App
